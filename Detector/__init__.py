52f8e08934297fcddbb55a61c7013514df11d6f1
Marc Lichtman <marclichtman@microsoft.com>
2023-01-20 22:01:40 -0500

    Fork from microsoft/IQEngine post-sprinternship

---
+++ Detector/__init__.py
@@ -0,0 +1,81 @@
+import logging
+import azure.functions as func
+import fastapi
+import numpy as np
+from fastapi.middleware.cors import CORSMiddleware
+
+app = fastapi.FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/sample")
+async def index():
+    return {
+        "info": "Try /hello/Shivani for parameterized route.",
+    }
+
+
+@app.get("/hello/{name}")
+async def get_name(name: str):
+    return {
+        "name": name,
+    }
+
+@app.get("/detect/{detectorname}")
+async def get_detect(detectorname: str):
+    logging.info("got here")
+    try:
+        detect_func = getattr(__import__(detectorname, fromlist=["detect"]), "detect")
+        logging.info("loaded detector")
+    except ModuleNotFoundError as e:
+        return {"status" : "FAILED - detector does not exist", "annotations": []}
+    return {
+        "detectorname": detectorname,
+    }
+
+
+@app.post("/detect/{detectorname}")
+async def detect(info : fastapi.Request, detectorname):
+    try:
+        detect_func = getattr(__import__(detectorname, fromlist=["detect"]), "detect")
+        logging.info("loaded detector")
+    except ModuleNotFoundError as e:
+        return {"status" : "FAILED - detector does not exist", "annotations": []}
+
+    function_input = await info.json()
+    samples = function_input["samples"] # Assumed to be real or floats in IQIQIQIQ (cant send complex over JSON)
+    if not samples:
+        return {
+        "status" : "FAILED",
+        "annotations" : []
+    }
+    samples = np.asarray(samples)
+    if samples.size % 2 == 1: # in case it comes in odd just remove the last element
+        samples = samples[:-1]
+    samples = samples[::2] + 1j*samples[1::2]
+    samples = samples.astype(np.complex64)
+    annotations = detect_func(samples,
+                              function_input.get("sample_rate", 1),
+                              function_input.get("center_freq", 0),
+                              function_input.get("detector_settings",{}))
+    logging.info(annotations)
+    return {
+        "status" : "SUCCESS",
+        "annotations" : annotations
+    }
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(app).handle_async(req, context)
---
+++ README.md
@@ -0,0 +1,1 @@
+Code deployed as Azure Functions to run the detector backend and thumbnail generator (blob storage triggered)
